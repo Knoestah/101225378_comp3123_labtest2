1434fbe22392d8674418e94888451ce67c90f70f
Knoester <free.knoester@gmail.com>
2021-11-29 23:45:40 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { render } from "react-dom";
+import axios from "axios";
 
-function App() {
+const WeatherApp = () => {
+  const [temperature, setTemperature] = useState("");
+  const [desc, setDesc] = useState("");
+  const [city, setCity] = useState("Toronto");
+  const [country, setCountry] = useState("Canada");
+
+  const getWeatherData = (city, country) => {
+    axios({
+      method: "GET",
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=180941f68139fba12f166dc35d9b688b`,
+    })
+      .then((response) => {
+        console.log(response.data.main.temp);
+        // Kelvin to Fahrenheit
+        // setTemperature((response.data.main.temp - 273.15) * 1.8 + 32);
+
+        // Kelvin to Celsius
+        setTemperature(response.data.main.temp - 273.15);
+        // console.log(response.data);
+        setDesc(response.data.weather[0].main);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "70px",
+          width: "100%",
+          backgroundColor: "#FFA500",
+          fontSize: "50px",
+          color: "#fff",
+        }}
+      >
+        Weather
+      </div>
+      {<div
+        style={{ height: "5px", width: "100%", backgroundColor: "blue" }}
+      ></div>}
+      <br />
+      <div style={{ marginLeft: "37%" }}>
+        <div
+          style={{
+            height: "150px",
+            width: "450px",
+            backgroundColor: "#808080",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            fontSize: "25px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {new Date().toLocaleString()}
+          <br />
+          {city} Weather
+          <br />          
+          {Math.round(temperature * 100) / 100} ℃ - {desc}
+        </div>
+        <br />
+        <input
+          type="text"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <input
+          type="text"
+          value={country}
+          onChange={(e) => setCountry(e.target.value)}
+        />
+        <button
+          onClick={() => {
+            getWeatherData(city, country);
+          }}
+        >
+          GET
+        </button>
+      </div>
+    </>
   );
-}
+};
 
-export default App;
+render(<WeatherApp />, document.querySelector("#root"));
+
+export default WeatherApp;
